fca47bb9ce28439895b1e66e9bb961c4121b0ff8
Reiii3 <mohtoyib79@gmail.com>
2025-02-11 19:21:14 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,15 @@ $AXFUN
 import axeron.prop
 runPackage1="com.rezone.gvortex"
 
+if [ -n "$1" ] && [ "$1" == "-p" ];then
+    axprop $path_axeronprop runPackage -s "$2"
+    runPackage="$2"
+    shift 2
+fi
+
 if [ -z $runPackage ]; then 
     echo "package not detected"
+    exit 1
 fi
 
 echo "=======================================" 
