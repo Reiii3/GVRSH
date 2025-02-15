bf7299eb542fe6f22036d68284df972f0d9d157e
Reiii3 <mohtoyib79@gmail.com>
2025-02-15 20:30:50 +0700

    update rmv.sh

--- rmv.sh
+++ rmv.sh
@@ -17,9 +17,9 @@ sleep 0.5
 echo "    Update    : none "
 sleep 0.5
 if [ -f "$log_file" ]; then
-  echo "    Status    : [Non Active]"
-else 
   echo "    Status    : [Active]"
+else  
+  echo "    Status    : [Non Active]"
 fi
 sleep 1
 echo "======================================="
