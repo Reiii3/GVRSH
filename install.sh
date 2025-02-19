580f0f38a19788bd6de3e200ef9c5b1b966816a7
Reiii3 <mohtoyib79@gmail.com>
2025-02-19 19:48:54 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,45 @@ fle_ins() {
 }
 
 apply () {
- 
+ # Kode Optimasi Performa untuk Helio G35 ke Bawah
+
+setprop debug.sf.hw 1
+setprop debug.egl.hw 1
+setprop debug.egl.sync 0
+setprop debug.cpuprio 6
+setprop debug.gpuprio 7
+setprop debug.ioprio 7
+setprop debug.performance.tuning 1
+setprop debug.performance.profile 1
+setprop debug.multicore.processing 1
+setprop debug.systemuicompilerfilter speed
+
+# Kode Disable AA (Anti-Aliasing) untuk Stabilitas GPU Lemah
+setprop debug.egl.force_fxaa false
+setprop debug.egl.force_taa false
+setprop debug.egl.force_msaa false
+setprop debug.egl.force_ssaa false
+setprop debug.egl.force_smaa false
+setprop debug.egl.force_mlaa false
+setprop debug.egl.force_txaa false
+setprop debug.egl.force_csaa false
+
+# Kode Support Perf/Render
+setprop debug.renderengine.backend skiavk
+setprop debug.hwui.render_performance true
+cmd thermalservice override-status 0
+cmd power set-adaptive-power-saver-enabled false
+cmd power set-mode 0
+
+# Kode Render Perf
+setprop debug.hwui.disable_draw_defer true
+setprop debug.hwui.disable_draw_reorder true
+setprop debug.hwui.render_dirty_regions false
+setprop debug.hwui.skip_empty_damage true
+setprop debug.hwui.disable_vsync true
+setprop debug.hwui.fps_divisor 1
+setprop debug.hwui.render_thread true
+setprop debug.hwui.render_thread_count 1
 }
 
 apply2 () {
