c3141e965d586bcafa0d9529c8187f39351bb284
Reiii3 <mohtoyib79@gmail.com>
2025-02-20 08:20:39 +0700

    update rmv.sh

--- rmv.sh
+++ rmv.sh
@@ -7,23 +7,37 @@ import axeron.prop
 log_folder="/data/local/tmp/axeron_cash/zcek_ins"
 log_file="$log_folder/install_selesai"
 
+printer() {
+text="$1"
+ i=0
+   while [ $i -lt ${#text} ]; do
+     echo -en "${text:$i:1}"
+ sleep 0.02
+   i=$((i + 1))
+ done
+echo
+}
+
 echo "=======================================" 
-echo "     Welcome To Modules Plugin GVR "
+sleep 0.5
+printer "     Welcome To Modules Plugin GVR "
+sleep 0.5
 echo "=======================================" 
 sleep 0.5
-echo "    Developer : Reii"
+printer "    Developer : Reii"
 sleep 0.5
-echo "    Version   : ${version} | ${versionCode}"
+printer "    Version   : ${version} | ${versionCode}"
 sleep 0.5
-echo "    Update    : none "
+printer "    Update    : none "
 sleep 0.5
 if [ -f "$log_file" ]; then
-  echo "    Status    : [Active]"
+  printer "    Status    : [Active]"
 else  
-  echo "    Status    : [Non Active]"
+  printer "    Status    : [Non Active]"
 fi
-sleep 1
+sleep 0.5
 echo "======================================="
+sleep 0.7
 echo
 echo " [Remove Game: ${nameGame}]"
 sleep 0.5
@@ -31,7 +45,9 @@ echo
 
 if [ -d "$log_folder" ]; then
      echo "      =================================="
-     echo "          [Disabling System Modules]"
+     sleep 0.5
+     printer "          [Disabling System Modules]"
+     sleep 0.5
      echo "      =================================="
      sleep 1
      rm -rf $log_folder
@@ -39,17 +55,24 @@ fi
 
 if [ ! -d "$log_folder" ]; then
      echo "      =================================="
-     echo "         Update Status : [Non Active]"
+     sleep 0.5
+     printer "         Update Status : [Non Active]"
+     sleep 0.5
      echo "      =================================="
 else
-     echo "      Update Status : [Failed Disabled]"
-     echo "      =================================="
+     echo "      ==================================="
+     sleep 0.5
+     printer "       Update Status : [Failed Disabled]"
+     sleep 0.5
+     echo "      ==================================="
 fi
 echo
 sleep 1
 
 echo " ============================"
-echo "   Disable Game Adjustments"
+sleep 0.5
+printer "   Disable Game Adjustments"
+sleep 0.5
 echo " ============================"
 sleep 1
 echo
@@ -63,5 +86,6 @@ echo " [$nameGame]->[Removed All Fetures]"
 sleep 1
 echo
 
-echo "| Thank you for using my module |"
-echo "             Enjoy "
+printer "| Thank you for using my module |"
+sleep 0.5
+printer "             Enjoy "
