c354a183950bc6e0dfbc94163500c95492b3a57d
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 21:31:13 +0700

    update function/function.sh

--- function/function.sh
+++ function/function.sh
@@ -1,18 +1,18 @@
 printer() {
-    text="$1"
-    i=0
-    while [ $i -lt ${#text}; do
-        echo -en "${text:$i:1}"
-        sleep 0.02
-        i=$((i + 1))
-    done
-    echo
+text="$1"
+ i=0
+   while [ $i -lt ${#text} ]; do
+     echo -en "${text:$i:1}"
+ sleep 0.02
+   i=$((i + 1))
+ done
+echo
 }
 
 fle_ins() {
-    folder_path="/data/local/tmp/axeron_cash/xcek_ts"
-    file_name="$1"
+    folder_path="/data/local/tmp/axeron_cash/zcek_ins"
+    file_name="$1"    # Nama fil
+    # Buat file di dalamnya
     file_path="$folder_path/$file_name"
     echo "" > "$file_path"
 }
-
